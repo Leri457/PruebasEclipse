a9639064967e371cb2a0a76d170ad933866e1f6f
Leri457 <126875726+Leri457@users.noreply.github.com>
2023-03-28 10:36:48 +0200

    Update Ordenador.java

--- ordenador_git/src/ordenador_git/Ordenador.java
+++ ordenador_git/src/ordenador_git/Ordenador.java
@@ -24,7 +24,7 @@ public class Ordenador {
 	public Ordenador() {}
 	
 	/**
-	 * Constructor de la clase al que se le pasan
+	 *
 	 * @param modelo: modelo del ordenador
 	 * @param precio: precio del ordenador
 	 * @param memoria: tamaño de la memoria del ordenador
